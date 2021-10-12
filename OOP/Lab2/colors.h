bf1547ab054678076122c927fc937e8b359381db
Andrey Kurzantsev <hawkeye54123@gmail.com>
2021-10-12 21:19:40 +0300

    add lab2

---
+++ OOP/Lab2/colors.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "pch.h"
+
+static COLORREF orange = RGB(255, 128, 0);
+static COLORREF pink = RGB(255, 0, 255);
+static COLORREF white = RGB(255, 255, 255);
+static COLORREF black = RGB(0, 0, 0);
